18283df1e4edb7d73bc35acfa70ea9a8a380955a
Manuel Roglan <manueljoseph113@gmail.com>
2025-02-01 16:36:59 -0600

    sandbox nav bar

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -2,9 +2,18 @@ import MainFooter from "@/components/nav/MainFooter";
 import MainHeader from "@/components/nav/MainHeader";
 // import Main from "@/components/sandbox/Main2";
 import Main from "@/components/sandbox/Main";
+import { mustNotBeAuthenticated } from "@/utils/auth";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
+import { parseCookies } from "nookies";
 
-export default function Sandbox() {
+
+interface Props {
+    isLoggedIn: boolean;
+}
+
+
+export default function Sandbox({isLoggedIn}: Props) {
 
     return (
         <>
@@ -12,10 +21,20 @@ export default function Sandbox() {
                 <title>Sandbox!</title>     
             </Head> 
             <div className="root-header-footer">
-                <MainHeader />
+                <MainHeader loggedIn={isLoggedIn} />
                 <Main />
                 <MainFooter />
             </div>
         </>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+
+    const token = parseCookies(ctx)["user-auth"]
+
+    const isLoggedIn = Boolean(token)
+
+    return {props: {isLoggedIn}}
 }
