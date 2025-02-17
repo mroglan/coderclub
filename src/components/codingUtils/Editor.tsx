0bf739d8ebb7fc037390bae12637dcc1f979bef9
Manuel Roglan <manueljoseph113@gmail.com>
2025-02-17 14:19:46 -0600

    drag items in editor

--- src/components/codingUtils/Editor.tsx
+++ src/components/codingUtils/Editor.tsx
@@ -1,20 +1,120 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Chip, Grid2, Paper, Typography } from "@mui/material";
 import { Dispatch, RefObject, SetStateAction, useEffect, useMemo, useRef, useState } from "react";
 import { EditorView, keymap } from "@codemirror/view";
 import { indentMore, indentLess } from "@codemirror/commands";
 import { python } from "@codemirror/lang-python";
 import { EditorState } from "@codemirror/state";
 import { basicSetup } from "codemirror";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+
+const DRAG_COLORS = {
+    Common: "#cd43e6",
+    Casting: "#4966eb"
+}
+
+
+const DRAG_TYPES = {
+    Common: ["print", "input"],
+    Casting: ["str", "int"]
+}
+
+
+const DRAG_ITEMS = {
+    print: {
+        text: "print()",
+    },
+    input: {
+        text: "input()",
+    },
+    str: {
+        text: "str()",
+    },
+    int: {
+        text: "int()",
+    }
+}
+
+
+interface DraggableChipProps {
+    type: string;
+    item: {
+        text: string;
+    }
+}
+
+
+function DraggableChip({type, item}: DraggableChipProps) {
+
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "DragItem",
+        item, // Pass a copy of the item
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging(),
+        }),
+    }));
+
+    
+
+    return (
+        <Chip ref={drag as any} label={item.text} style={{
+            borderColor: DRAG_COLORS[type],
+            color: DRAG_COLORS[type],
+            cursor: "pointer",
+            fontSize: "1.5rem",
+            padding: "16px 16px"
+        }} variant="outlined" />
+    )
+}
+
+
+interface DroppableEditorProps {
+    editorViewRef: RefObject<EditorView|null>;
+    editorRef: RefObject<HTMLDivElement|null>;
+    height?: string;
+}
+
+
+function DroppableEditor({editorViewRef, editorRef, height}: DroppableEditorProps) {
+
+    const [, drop] = useDrop(() => ({
+        accept: "DragItem",
+        drop: (item: any, monitor) => {
+            const {x, y} = monitor.getClientOffset()
+            const pos = editorViewRef.current.posAtCoords({x, y})
+            const t = editorViewRef.current.state.update({
+                changes: {
+                    from: pos,
+                    insert: item.text
+                },
+                selection: {
+                    anchor: pos,
+                    head: pos + item.text.length
+                },
+
+            })
+            editorViewRef.current.dispatch(t)
+        },
+    }));
+
+    return (
+        <Box ref={drop as any}>
+            <div ref={editorRef} style={{height: height || "500px"}} />
+        </Box>
+    )
+}
 
 
 interface DefaultEditorProps {
     originalCode: string;
     editorViewRef: RefObject<EditorView|null>;
     height?: string;
+    dragItems?: string[];
 }
 
 
-export function DefaultEditor({originalCode, editorViewRef, height}: DefaultEditorProps) {
+export function DefaultEditor({originalCode, editorViewRef, height, dragItems}: DefaultEditorProps) {
 
     const editorRef = useRef<HTMLDivElement>(null);
 
@@ -44,13 +144,52 @@ export function DefaultEditor({originalCode, editorViewRef, height}: DefaultEdit
         }
     }, []);
 
+    const dItems = useMemo(() => {
+        const l = dragItems || Object.keys(DRAG_ITEMS)
+        const d = {}
+        for (let key of Object.keys(DRAG_TYPES)) {
+            console.log('key', key)
+            d[key] = DRAG_TYPES[key].filter(k => l.includes(k)).map(k => DRAG_ITEMS[k])
+        }
+        return d
+    }, [dragItems])
+
     return (
         <Box>
-            <Paper elevation={5}>
-                <Box pt={1}>
-                    <div ref={editorRef} style={{height: height || "500px"}} />
-                </Box>
-            </Paper>
+            <DndProvider backend={HTML5Backend}>
+                <Paper id="my-dnd-root" elevation={5}>
+                    <Box pt={1}>
+                        <Grid2 container wrap="nowrap">
+                            <Grid2 width={200} sx={{overflowY: "scroll"}}>
+                                <Box ml={0.5}>
+                                    {Object.keys(dItems).map(key => {
+                                        if (dItems[key].length === 0) return null
+                                        return (
+                                            <Box key={key}>
+                                                <Box mb={1}>
+                                                    <Typography variant="body1" color={DRAG_COLORS[key]}>
+                                                        {key}
+                                                    </Typography>
+                                                </Box>
+                                                <Box mb={1} ml={1}>
+                                                    {dItems[key].map(item => (
+                                                        <Box key={item.text} mt={1}>
+                                                            <DraggableChip type={key} item={item} />
+                                                        </Box>
+                                                    ))}
+                                                </Box>
+                                            </Box>
+                                        )
+                                    })}
+                                </Box>
+                            </Grid2>
+                            <Grid2 flex={1}>
+                                <DroppableEditor editorRef={editorRef} editorViewRef={editorViewRef} height={height} />
+                            </Grid2>
+                        </Grid2>
+                    </Box>
+                </Paper>
+            </DndProvider>
         </Box>
     )
 }
@@ -59,7 +198,6 @@ export function DefaultEditor({originalCode, editorViewRef, height}: DefaultEdit
 interface EditorTabsProps {
     tabs: string[];
     selectedTab: string;
-    // setSelectedTab: Dispatch<SetStateAction<string>>;
     setSelectedTab: (tab: string) => void;
 }
 
