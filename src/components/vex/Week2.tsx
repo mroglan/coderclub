b785f84ca21b214309bdecec041d924e1b1544a6
Manuel Roglan <manueljoseph113@gmail.com>
2025-09-28 23:27:15 -0500

    update week 2

--- src/components/vex/Week2.tsx
+++ src/components/vex/Week2.tsx
@@ -24,19 +24,31 @@ export default function Week2() {
                 <Box mt={3}>
                     {path == "Coding Team" ? <Box>
                         <Box>
-                            <Typography>
-                                1. Go to <PrimaryLink href="https://vr.vex.com" target="_">vr.vex.com</PrimaryLink> Click on the "Students Get Started" button when the page opens, and follow the tour to learn about the environment!
+                            <Typography display="inline">
+                                1. Go to{' '}
                             </Typography> 
+                            <PrimaryLink href="https://vr.vex.com" target="_">vr.vex.com</PrimaryLink> 
+                            <Typography display="inline">
+                                {' '}Click on the "Students Get Started" button when the page opens, and follow the tour to learn about the environment!
+                            </Typography>
                         </Box>
                         <Box mt={2}>
-                            <Typography>
-                                2. Complete the <PrimaryLink href="https://docs.google.com/document/d/1izVTBW0AJeK3JR7a59CL0lA0ldfdoOWltfL0TicWHaI/edit?tab=t.0#heading=h.1fob9te" target="_">Distance Drive Challenge.</PrimaryLink> You will learn how to move your robot!
+                            <Typography display="inline">
+                                2. Complete the{' '}
                             </Typography> 
+                            <PrimaryLink href="https://docs.google.com/document/d/1izVTBW0AJeK3JR7a59CL0lA0ldfdoOWltfL0TicWHaI/edit?tab=t.0#heading=h.1fob9te" target="_">Distance Drive Challenge.</PrimaryLink>
+                            <Typography display="inline">
+                                {' '}You will learn how to move your robot!
+                            </Typography>
                         </Box>
                         <Box mt={2}>
-                            <Typography>
-                                3. Try to get your robot to do <PrimaryLink href="https://docs.google.com/document/d/1vNl_9wuVeRt91LvC0ADpCv4-HjO_g1Ly_H09sYEqhh0/edit?tab=t.0#heading=h.1fob9te" target="_">Basketball Drills!</PrimaryLink>. If you did coding club last semester, you might be able to use loops to make your code shorter.
+                            <Typography display="inline">
+                                3. Try to get your robot to do{' '}
                             </Typography> 
+                            <PrimaryLink href="https://docs.google.com/document/d/1vNl_9wuVeRt91LvC0ADpCv4-HjO_g1Ly_H09sYEqhh0/edit?tab=t.0#heading=h.1fob9te" target="_">Basketball Drills!</PrimaryLink>
+                            <Typography display="inline">
+                                {' '}If you did coding club last semester, you might be able to use loops to make your code shorter.
+                            </Typography>
                         </Box>
                         <Box mt={2}>
                             <Typography>
@@ -45,9 +57,15 @@ export default function Week2() {
                             <Typography pl={2}>
                                 Some of the tiles in the classroom have been marked with a token. Write a program that will make the robot drive to each tile.
                             </Typography>
-                            <Typography pl={2}>
-                                Once your program has been tested in the simulation, copy your code over to <PrimaryLink href="https://codeiq.vex.com/" target="_">codeiq.vex.com</PrimaryLink> We can then flash the robot with your program!
-                            </Typography>
+                            <Box pl={2}>
+                                <Typography display="inline">
+                                    Once your program has been tested in the simulation, copy your code over to{' '}
+                                </Typography>
+                                <PrimaryLink href="https://codeiq.vex.com/" target="_">codeiq.vex.com</PrimaryLink>
+                                <Typography display="inline">
+                                    {' '}We can then flash the robot with your program!
+                                </Typography>
+                            </Box>
                         </Box>
                     </Box> : <Box>
                         <Box>
@@ -61,9 +79,10 @@ export default function Week2() {
                             </Typography> 
                         </Box>
                         <Box mt={2}>
-                            <Typography>
-                                2. Follow the build instructions <PrimaryLink href="https://content.vexrobotics.com/stem-labs/iq/pdf/iq-2nd-gen-speedbot-v3.pdf" target="_">here!</PrimaryLink>
+                            <Typography display="inline">
+                                2. Follow the build instructions{' '}
                             </Typography>
+                            <PrimaryLink href="https://content.vexrobotics.com/stem-labs/iq/pdf/iq-2nd-gen-speedbot-v3.pdf" target="_">here!</PrimaryLink>
                         </Box>
                         <Box mt={2}>
                             <Typography>
